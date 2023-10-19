2ba16947ae22266d6715af61e1edebe843703ee2
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-10-19 12:35:51 -0600

    install: react-router, react-router-dom
      config router.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Router from 'src/router';
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode></React.StrictMode>
+	<React.StrictMode>
+		<Router />
+	</React.StrictMode>
 );
